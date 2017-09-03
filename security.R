a7a1127766bdcd4e3c7665b398acdae28daa7690
Humbert <humbert.costas@gmail.com>
2017-09-04 00:57:27 +0200

    Just one public function, getinfo?txt=

--- security.R
+++ security.R
@@ -1,29 +1,25 @@
-# Download latests data sets
-utils::download.file(url = "https://github.com/r-net-tools/security.datasets/raw/master/net.security/sysdata.rda",
-                     destfile = paste(tempdir(),"sysdata.rda",sep = "\\"))
-load(file = paste(tempdir(), "sysdata.rda", sep = "\\"))
-
-dds <- netsec.data$datasets
-rm(netsec.data)
-
-#' @get /cveinfo
-cveinfo <- function(cve = "CVE-2010-2010") {
-  dds$cves[cve == dds$cves$cve,]
+#' @get /getinfo
+getinfo <- function(txt = ""){
+  if (grepl(pattern = "^CVE-\\d+-\\d+$", x = txt)) {
+    # CVE
+    cves <- dds$cves[txt == dds$cves$cve,]
+    xplt <- exploitdb[grep(pattern = txt, x = exploitdb$cves, ignore.case = T),]
+    xplt <- xplt[order(as.Date.POSIXct(xplt$date), decreasing = T),]
+    info <- list(cves, xplt)
+  } else if (grepl(pattern = "^CWE-\\d+$", x = txt)) {
+    # CWE
+    info <- dds$cwes[txt == dds$cwes$code_standard,]
+  } else if (grepl(pattern = "^CAPEC-\\d+$", x = txt)) {
+    # CAPEC
+    info <- dds$capec[substring(txt,7,nchar(txt)) == dds$capec$id,]
+  } else {
+    # CPE or Exploit
+    cpes <- dds$cpes[agrep(pattern = tolower(txt), x = dds$cpes$title), c("cpe.23")]
+    print(cpes)
+    xplt <- exploitdb[agrep(pattern = txt, x = exploitdb$software, ignore.case = T),]
+    xplt <- xplt[order(as.Date.POSIXct(xplt$date), decreasing = T),]
+    info <- list(xplt, cpes)
+  }
+  return(info)
 }
 
-#' @get /cweinfo
-cweinfo <- function(cwe = "CwE-20") {
-  dds$cwes[cwe == dds$cwes$code_standard,]
-}
-
-#' @get /cpeinfo
-cpeinfo <- function(cpe = "winamp") {
-  cpe <- tolower(cpe)
-  dds$cpes[agrep(pattern = cpe, x = dds$cpes$title), c("cpe.23")]
-}
-
-#' @get /capecinfo
-cpeinfo <- function(capec = "CAPEC-300") {
-  capec <- substring(capec,7,nchar(capec))
-  dds$capec[capec == dds$capec$id,]
-}

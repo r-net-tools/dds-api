a5fa058648636d51850c803246d25381189f561d
Humbert <humbert.costas@gmail.com>
2017-09-04 14:31:25 +0200

    PoC: working in localhost

--- api.R
+++ api.R
@@ -1,3 +1,17 @@
 library("plumber")
+
+# Download latests data sets
+utils::download.file(url = "https://github.com/r-net-tools/security.datasets/raw/master/explpoitdb/exploitdb.rda",
+                     destfile = paste(tempdir(),"exploitdb.rda",sep = "\\"))
+load(file = paste(tempdir(), "exploitdb.rda", sep = "\\"))
+
+utils::download.file(url = "https://github.com/r-net-tools/security.datasets/raw/master/net.security/sysdata.rda",
+                     destfile = paste(tempdir(),"sysdata.rda",sep = "\\"))
+load(file = paste(tempdir(), "sysdata.rda", sep = "\\"))
+
+dds <- netsec.data$datasets
+rm(netsec.data)
+
+# Start API
 r <- plumb("security.R")
 r$run(port=8000)

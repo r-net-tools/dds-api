d94be52a7a05f3c1503a38eb72c5c2c624dd0f44
Humbert Costas <hcostas@ackcent.com>
2017-09-04 12:01:47 +0200

    Init api databases

--- api.R
+++ api.R
@@ -1,3 +1,14 @@
 library("plumber")
+
+utils::download.file(url = "https://github.com/r-net-tools/security.datasets/raw/master/net.security/sysdata.rda",
+                     destfile = paste(tempdir(),"sysdata.rda",sep = "\\"))
+load(file = paste(tempdir(), "sysdata.rda", sep = "\\"))
+dds <- netsec.data$datasets
+rm(netsec.data)
+
+utils::download.file(url = "https://github.com/r-net-tools/security.datasets/raw/master/explpoitdb/exploitdb.rda",
+                     destfile = paste(tempdir(),"exploitdb.rda",sep = "\\"))
+load(file = paste(tempdir(), "exploitdb.rda", sep = "\\"))
+
 r <- plumb("security.R")
-r$run(port=8000)
+r$run(port=7374)
